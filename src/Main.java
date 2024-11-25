ec1e64e444f94ee34182f96d6f1d0617805a220a
Kuke <marslevandreas2@gmail.com>
2024-11-25 11:14:39 +0100

    Added more to main Added displayList in MediaList
    Stuff not working:
    It cant fully display series yet.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         User user = new User();
+        MediaList mediaList = new MediaList("data/film.txt","data/serier.txt");
 
         while(true){
             TextUI.messagePrint("Welcome to Netflix & Chill");
@@ -17,8 +18,48 @@ public class Main {
 
             switch (choice){
                 case "1":
-                    user.loginUser();
-                    break;
+                    if (user.loginUser());
+                        boolean ifLogged = true;
+                        while (ifLogged) {
+                            TextUI.messagePrint("\nMain menu:");
+                            TextUI.messagePrint("1. Movies");
+                            TextUI.messagePrint("2. Series");
+                            TextUI.messagePrint("3. Search");
+                            TextUI.messagePrint("4. Watchlist");
+                            TextUI.messagePrint("5. Finished media");
+                            TextUI.messagePrint("6. Logout");
+
+                            String mainMenuChoice = TextUI.messagePrompt("Enter your choice:");
+
+                            switch (mainMenuChoice) {
+                                case "1":
+                                    TextUI.messagePrint("Displaying movies.");
+                                    mediaList.displayMediaList("Movies");
+                                    break;
+                                case "2":
+                                    TextUI.messagePrint("Displaying series");
+                                    mediaList.displayMediaList("Series");
+                                    break;
+                                case "3":
+                                    TextUI.messagePrint("You can search for a movie or genre");
+                                    break;
+                                case "4":
+                                    TextUI.messagePrint("Here is your watchlist");
+                                    break;
+                                case "5":
+                                    TextUI.messagePrint("List over your finished media");
+                                    break;
+                                case "6":
+                                    TextUI.messagePrint("Logging out");
+                                    ifLogged = false;
+                                    break;
+
+                                default:
+                                    TextUI.messagePrint("Invalid choice.");
+                            }
+                        }
+
+                        break;
                 case "2":
                     user.createUser();
                     break;
